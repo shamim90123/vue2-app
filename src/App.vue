18da5c094f732bcc3d477a9da5b8064c1b16673c
Md Shamim Reza <opt.shamim@gmail.com>
2024-01-13 06:37:28 +0600

    local and live seperate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Vue.js App"/>
+    <HelloWorld msg="Welcome to Vue.js App. Hi this is new"/>
   </div>
 </template>
 
